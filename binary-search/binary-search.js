d0e8e409db58488cb3d034701fd138dea9cc7c9d
Haley Sambursky <hsambursky46@gmail.com>
2019-10-23 16:30:31 -0400

    searches

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,24 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	let head = 0
+	let tail = array.length - 1
+	let midpoint = Math.floor((tail + head) / 2)
+
+	while (tail >= head) {
+		console.log("head", head, "tail", tail, "midpoint", midpoint, "target", target, "value", array[midpoint])
+		if (array[midpoint] === target) {
+			return true
+		} else if (array[midpoint] > target) {
+			tail = midpoint - 1
+			midpoint = Math.floor((tail + head) / 2)
+		} else {
+			head = midpoint + 1
+			midpoint = Math.floor((tail + head) / 2)
+		}
+	}
+
+	return false
 };
 
 /*
